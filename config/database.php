2a9be6e2484bcf6f71292a86b9d9f5ef955fc2a4
atifformateur <atif.zourgani@laplateforme.io>
2025-08-06 15:30:53 +0200

    mise en place de la logique de connexion pdn, dsn

--- config/database.php
+++ config/database.php
@@ -1,2 +1,32 @@
 <?php 
 
+// logique de connexion a la database
+
+//information pour se connecter
+//l'endroit ou est ma database
+$host = "localhost";
+//le nom de la db
+$dbname = "users_db";
+//identifiant de connexion
+$username = "root";
+//mdp de connexion
+$password = "";
+//port
+$port = 3306;
+//encodage
+$charset = "utf8mb4";
+
+//fonction qui crée et renvoi une connexion a la db
+function dbConnexion() {
+    //transforme mes variable en global (accessible partout)
+    global $host, $dbname, $password, $username, $port, $charset;
+
+    try {
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
+        
+    } catch () {
+        
+    }
+    
+}
+
