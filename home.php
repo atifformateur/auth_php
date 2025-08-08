798ecdb4b2c70baebec24c633c27d8636aea38b1
atifformateur <atif.zourgani@laplateforme.io>
2025-08-08 11:45:08 +0200

    access control page sécurisés

--- home.php
+++ home.php
@@ -1,12 +1,30 @@
 <?php 
+//demarre la session
 session_start();
 
+// condition pour verifier si dans la session de l'utilisateur se trouve les infos qui nous montre qu'il est co 
 if (!isset($_SESSION['loggin']) || $_SESSION['loggin'] !== true ) {
-
+    //si il est co je le redirige grace a location, qui dans le header de ma request
     header('location: login.php');
+    //je lui dis du coup d'arreter d'executer le code
     exit();
 }
 
+//condition qui me permet de verifier si en get j'ai une key logout ET si cette key contient la value 1 ( true )
+if (isset($_GET['logout']) && $_GET['logout'] === '1') {
+    $_SESSION = [];
+    session_destroy();
+
+    //supprime les cookie
+
+    //si il est deco on le renvois sur la pas de logout surtout qu'il a pas le droit de rester ici 
+    header('location: login.php');
+    //je lui dis du coup d'arreter d'executer le code
+    exit();
+
+}
+
+//(get) je recupere les infos stocké dans le tableau de la session du user 
 $user_id = $_SESSION['user_id'] ?? 'inconnu';
 $username = $_SESSION['username'] ?? 'non renseigné';
 $email = $_SESSION['email'] ?? "pas d'email";
@@ -23,11 +41,12 @@ $email = $_SESSION['email'] ?? "pas d'email";
 <body>
     <header>
         <h1>mon site d'auth en php</h1>
-        <span><?= "welcom $username sur mon site. Vous êtes co avec l'adresse mail $email"; ?></span>
+        <!-- petit message personnalisé grace aux infos stocké dans la session  -->
+        <span><?= "welcom " . htmlspecialchars($username) . "sur mon site. Vous êtes co avec l'adresse mail " . htmlspecialchars($email); ?></span>
         <nav>
             <ul>
                 <li>
-                    <a href="">lien vers une page</a>
+                    <a href="home.php">home page</a>
                 </li>
                 <li>
                     <a href="">lien vers une page</a>
@@ -36,7 +55,7 @@ $email = $_SESSION['email'] ?? "pas d'email";
                     <a href="">lien vers une page</a>
                 </li>
                 <li>
-                    <a href="">deco</a>
+                    <a href="home.php?logout=1">deco</a>
                 </li>
             </ul>
         </nav>
